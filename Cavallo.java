945ab8ecc981233e3129817090b593c131eaf604
Alberton Francesco <papo.paf2006@gmail.com>
2024-11-10 17:51:17 +0100

    implementazione metodo run

--- Cavallo.java
+++ Cavallo.java
@@ -1,17 +1,39 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Cavallo extends Thread{
     private String nome;
     private int distanza;
     private int fine;
     private static int distmax = 10;
+    private int velocita;
+    private boolean infortunato = false; 
+    private long tempoFine; 
     
-    public Cavallo(String nome, int fine){
+    public Cavallo(String nome, int fine, int velocita){
         this.nome = nome;
         this.fine = fine;
         this.distanza=0;
+        this.velocita= velocita;
     }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public long getTempoFine() {
+        return tempoFine;
+    }
+
     @Override
     public void run(){
-        while (distanza < fine){
+        long inizioTempo= System.currentTimeMillis();
+        while (distanza < fine&& !infortunato){
+            if(Math.random() < 0.1){
+                infortunato= true;
+                System.out.println(nome+"si è infortunato.");
+                break;
+            }
             int step = (int) (Math.random()*distmax)+1;
             distanza+=step;
             if (distanza> fine){
@@ -25,6 +47,10 @@ public class Cavallo extends Thread{
             e.printStackTrace();
         }
         }
-        System.out.println(nome + " ha finito la gara ");
+        if(!infortunato){
+            tempoFine = System.currentTimeMillis()-inizioTempo;
+            System.out.println(nome + " ha finito la gara in "+tempoFine+" ms.");
+        }
+        
     }
     }
